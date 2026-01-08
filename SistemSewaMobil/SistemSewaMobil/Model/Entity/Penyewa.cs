cc6f59dc26be02dbece95c964f452689e3cfb2b1
hanoremma <emmahalimah049@gmail.com>
2026-01-08 13:52:54 +0700

    initial commit

---
+++ SistemSewaMobil/SistemSewaMobil/Model/Entity/Penyewa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemSewaMobil.Model.Entity
+{
+    public class Penyewa
+    {
+        public string idPenyewa { get; set; }
+        public string namaPenyewa { get; set; }
+        
+    }
+}
